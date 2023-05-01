40c65342ac869eb849717c4a67cf53a2d8d456f9
pavnesh.saini <pavnesh.saini@altruistindia.com>
2023-05-01 11:54:51 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  env: {
+    MYSQL_HOST: "127.0.0.1",
+    MYSQL_PORT: "3306",
+    MYSQL_DATABASE: "test",
+    MYSQL_USER: "root",
+    MYSQL_PASSWORD: "",
+  },
+};
